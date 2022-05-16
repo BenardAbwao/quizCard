1b3f06a1e38f079fd8b1a2e1d1193419a94a1e5c
BenardAbwao <benard.okoth.abwao@gmail.com>
2022-05-16 10:19:29 +0300

    update quiz.html and index.js

--- js/index.js
+++ js/index.js
@@ -1,2 +1,66 @@
-//getting all required elements
-const btnstart =document.querySelector(".btnstart button")
+document.getElementsByClassName("questions")[0].style.display="block";
+
+function next1() {
+    document.getElementsByClassName("questions")[0].style.display = "none";
+    document.getElementsByClassName("questions")[1].style.display = "block";
+    document.getElementsByClassName("questions")[2].style.display = "none";
+    document.getElementsByClassName("questions")[3].style.display = "none";
+    document.getElementsByClassName("questions")[4].style.display = "none";
+    return false;
+}
+function next2() {
+    document.getElementsByClassName('questions')[0].style.display = "none";
+    document.getElementsByClassName('questions')[1].style.display = "none";
+    document.getElementsByClassName('questions')[2].style.display = "block";
+    document.getElementsByClassName('questions')[3].style.display = "none";
+    document.getElementsByClassName('questions')[4].style.display = "none";
+    return false;
+}
+function next3() {
+    document.getElementsByClassName("questions")[0].style.display = "none";
+    document.getElementsByClassName("questions")[1].style.display = "none";
+    document.getElementsByClassName("questions")[2].style.display = "none";
+    document.getElementsByClassName("questions")[3].style.display = "block";
+    return false;
+}
+function next4() {
+    document.getElementsByClassName('questions')[0].style.display = "none";
+    document.getElementsByClassName('questions')[1].style.display = "none";
+    document.getElementsByClassName('questions')[2].style.display = "none";
+    document.getElementsByClassName('questions')[3].style.display = "none";
+    document.getElementsByClassName('questions')[4].style.display = "block";
+    document.getElementsByClassName('submitClass')[0].style.display ="block";
+    return false;
+}
+function prev1() {
+    document.getElementsByClassName("questions")[0].style.display = "block";
+    document.getElementsByClassName("questions")[1].style.display = "none";
+    document.getElementsByClassName("questions")[2].style.display = "none";
+    document.getElementsByClassName("questions")[3].style.display = "none";
+    document.getElementsByClassName("questions")[4].style.display = "none";
+    return false;
+}
+function prev2() {
+    document.getElementsByClassName('questions')[0].style.display = "none";
+    document.getElementsByClassName('questions')[1].style.display = "block";
+    document.getElementsByClassName('questions')[2].style.display = "none";
+    document.getElementsByClassName('questions')[3].style.display = "none";
+    document.getElementsByClassName('questions')[4].style.display = "none";
+    return false;
+}
+function prev3() {
+    document.getElementsByClassName("questions")[0].style.display = "none";
+    document.getElementsByClassName("questions")[1].style.display = "none";
+    document.getElementsByClassName("questions")[2].style.display = "block";
+    document.getElementsByClassName("questions")[3].style.display = "none";
+    return false;
+}
+function prev4() {
+    document.getElementsByClassName('questions')[0].style.display = "none";
+    document.getElementsByClassName('questions')[1].style.display = "none";
+    document.getElementsByClassName('questions')[2].style.display = "none";
+    document.getElementsByClassName('questions')[3].style.display = "block";
+    document.getElementsByClassName('questions')[4].style.display = "none";
+    document.getElementsByClassName('submitClass')[0].style.display ="block";
+    return false;
+}
