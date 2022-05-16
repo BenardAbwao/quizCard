4d286b316e5d1075b3b201d968ae4c5d649c0919
BenardAbwao <benard.okoth.abwao@gmail.com>
2022-05-16 22:03:30 +0300

    add function to script.js, update classes to quiz.html

---
+++ js/script.js
@@ -0,0 +1,28 @@
+function check() {
+    var c=0
+    var q1=document.choice.q1.value;
+    var q2=document.choice.q2.value;
+    var q3=document.choice.q3.value;
+    var q4=document.choice.q4.value;
+    var q5=document.choice.q5.value;
+    if (q1=="Hypertext Transfer Protocol") {c++}
+    if (q2=="HTML") {c++}
+    if (q3=="Styling") {c++}
+    if (q4=="question") {c++}
+    if (q5=="mkdir") {c++}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
