e16c97c558bab1c628665e9fb5eef352d72baa2f
BenardAbwao <benard.okoth.abwao@gmail.com>
2022-05-16 22:30:32 +0300

    add functions to script.js and update quiz.html

--- js/script.js
+++ js/script.js
@@ -5,11 +5,21 @@ function check() {
     var q3=document.choice.q3.value;
     var q4=document.choice.q4.value;
     var q5=document.choice.q5.value;
+    var result=document.getElementById('result');
+    var choices=document.getElementById('choices');
     if (q1=="Hypertext Transfer Protocol") {c++}
     if (q2=="HTML") {c++}
     if (q3=="Styling") {c++}
     if (q4=="question") {c++}
     if (q5=="mkdir") {c++}
+
+    if (c<=4) {
+        result.textContent='Your result is ${c}. You can do better'
+    }   else {
+        result.textContent='Your result is ${c}. Excellent!'
+    }
+
+
 
 
 
